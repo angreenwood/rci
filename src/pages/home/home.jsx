4c71f3d27b06d69a0dfa2626c0abb9298af2e94c
DESKTOP-VTORIKK\Rapido <ghezts@gmail.com>
2023-08-06 18:04:12 -0400

    adding homepage mockup with logo

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import "./home.scss";
+
 const home = () => {
   return (
     <section className="content-background dark:bg-gray-900">
-      <div>
-        <p>home</p>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-left lg:mb-16 mb-8">
+          <img src="./pictures/rci-blue.svg" alt="Main-Logo" className="logo" />
+          <p className="font-light text-white sm:text-xl dark:text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div className="mx-auto max-w-screen-sm text-left lg:mb-16 mb-8">
+          <p className="font-light text-white sm:text-xl dark:text-gray-400">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     </section>
   );
