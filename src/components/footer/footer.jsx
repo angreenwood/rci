bb5f1643507bd79ef7168ec555101a4dd43dc8ce
DESKTOP-VTORIKK\Rapido <ghezts@gmail.com>
2023-08-07 20:53:43 -0400

    remove about + contact from footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer class="content-background m-4 dark:bg-gray-800">
-      <div class="w-full mx-auto max-w-screen-xl pl-4 md:flex md:items-center md:justify-between">
+      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
           <a href="https://somesit.com/" class="hover:underline">
@@ -12,11 +12,6 @@ function Footer() {
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">
-              About
-            </a>
-          </li>
-          <li>
             <a href="#" class="mr-4 hover:underline md:mr-6">
               Privacy Policy
             </a>
@@ -24,11 +19,6 @@ function Footer() {
           <li>
             <a href="#" class="mr-4 hover:underline md:mr-6">
               Licensing
-            </a>
-          </li>
-          <li>
-            <a href="#" class="hover:underline">
-              Contact
             </a>
           </li>
         </ul>
