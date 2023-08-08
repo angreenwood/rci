e1f28227474ff00983308ee601599d4e4f438a2a
DESKTOP-VTORIKK\Rapido <ghezts@gmail.com>
2023-08-07 20:36:38 -0400

    footer padding adjustment

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,14 +2,13 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer class="content-background rounded-lg shadow m-4 dark:bg-gray-800">
-      <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+    <footer class="content-background m-4 dark:bg-gray-800">
+      <div class="w-full mx-auto max-w-screen-xl pl-4 md:flex md:items-center md:justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{" "}
           <a href="https://somesit.com/" class="hover:underline">
             RCI Structural Engineering™
           </a>
-          . All Rights Reserved.
         </span>
         <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
