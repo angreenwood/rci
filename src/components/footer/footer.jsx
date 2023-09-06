6e1a7417cb6abd785ef224b0ebe905e6cb1c1c9b
DESKTOP-VTORIKK\Rapido <ghezts@gmail.com>
2023-09-06 11:22:43 -0400

    footer border

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="text-center footer">
+    <footer className="text-center footer border-t-2 border-blue-900">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
         <span className="text-sm text-gray-500 sm:text-center lg:mr-5">
           © 2023{" "}
